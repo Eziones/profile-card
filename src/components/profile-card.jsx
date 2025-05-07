698ac1867bd8087ba1291ab34a0b0c910844ae36
Eziones <romaincastro@gmail.com>
2025-05-07 13:52:49 +0200

    added a bit of a11y

--- src/components/profile-card.jsx
+++ src/components/profile-card.jsx
@@ -8,20 +8,20 @@ const ProfileCard = () => {
   return (
     <div className="profile-card">
       <div className="image-wrapper">
-        <img src={heroImage} />
+        <img src={heroImage} alt="Photo de profil de Monsieur Maou" />
       </div>
       <div className="hero-details">
         <h1>Monsieur Maou</h1>
         <p>Maou</p>
         <div className="social-links">
-          <a href="https://x.com/Ezionis" target="_blank">
-            <FontAwesomeIcon className="link-icon" icon={faTwitter} size={linksSize} />
+          <a href="https://x.com/Ezionis" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon className="link-icon" icon={faTwitter} size={linksSize} aria-label="Twitter" />
           </a>
-          <a href="https://x.com/Ezionis" target="_blank">
-            <FontAwesomeIcon className="link-icon" icon={faInstagram} size={linksSize} />
+          <a href="https://x.com/Ezionis" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon className="link-icon" icon={faInstagram} size={linksSize} aria-label="Instagram" />
           </a>
-          <a href="https://github.com/Eziones" target="_blank">
-            <FontAwesomeIcon className="link-icon" icon={faGithub} size={linksSize} />
+          <a href="https://github.com/Eziones" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon className="link-icon" icon={faGithub} size={linksSize} aria-label="GitHub" />
           </a>
         </div>
       </div>
